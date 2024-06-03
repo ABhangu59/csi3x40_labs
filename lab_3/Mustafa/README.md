935c9f238c1c75f5dcff9053e85c55e9a9a36a1c
Mustafa Ahmed <mustafa.ahmed1118@gmail.com>
2024-06-02 20:48:57 -0400

    added documentation

---
+++ lab_3/Mustafa/README.md
@@ -0,0 +1,21 @@
+# Lab 3: 1D Pac-Man
+
+Student: Mustafa Ahmed\
+Class: CSI3104\
+Date: June 2, 2024
+
+This is an implementation of 1D Pac-Man using Javascript to run the game and HTML to render it on a browser. It allows the user to control pacman, eat pellets and track their score.
+
+## How to Run:
+
+1. Open "pacman.html"
+2. Game begins upon opening in browser
+3. Move Pacman with Left and Right arrow keys
+
+## Score System:
+
+Eating a pellet increases the score by 1. When all pellets are eaten, you will proceed to the next level. The next level has 3 more spaces than the previous. The score continues to build accross each level.
+
+## Ghost Behaviour
+
+In the current implementation, the ghost moves randmoly left and right. The ghost cannot kill pacman as that was an optional feature of this assignment.
--- lab_3/Mustafa/pacman.js
+++ lab_3/Mustafa/pacman.js
@@ -1,17 +1,27 @@
+/*
+Mustafa Ahmed
+CSI3140 Lab 3 - 1D Pacman
+June 2, 2024
+*/
+
+
+//Define Game pieces
 const PACMAN = "C";
 const GHOST = "^";
 const FRUIT = "@";
 const PELLET = ".";
 
-var game = [];
+
+var game = []; //gameboard
 var gameWin = 0;
-var score = 0;
-var pellets = 0;
+var score = 0; //continues until session ends
+var pellets = 0; //resets after each game
 
 function createGame(n) {
 gameArray = [];
 pellets = 0;
 gameWin = n - 3; //all pellets subract other game pieces
+
 for (let i = 0; i < n; i++) {
     //spawn pacman in the middle
     if (i == Math.ceil(n / 2)) {
@@ -34,9 +44,12 @@ return gameArray;
 }
 
 function moveLeft(game) {
+
+//Locate Pacman
 indexPacMan = game.indexOf(PACMAN);
 indexLeftPacMan = 0;
-if (indexPacMan == 0) {
+
+if (indexPacMan == 0) {//Far left edge case
     indexLeftPacMan = game.length - 1;
 } else {
     indexLeftPacMan = indexPacMan - 1;
@@ -44,7 +57,7 @@ if (indexPacMan == 0) {
 
 left_val = game[indexLeftPacMan];
 
-if (left_val == PELLET) {
+if (left_val == PELLET) {//Eat a pellet
     processPellet(game, indexLeftPacMan, indexPacMan);
 } else {
     //swap
@@ -58,97 +71,108 @@ return game;
 }
 
 function moveRight(game) {
-indexPacMan = game.indexOf(PACMAN);
-indexRightPacMan = 0;
 
-if (indexPacMan == game.length - 1) {
-    indexLeftPacMan = 0;
-} else {
-    indexRightPacMan = indexPacMan + 1;
-}
-right_val = game[indexRightPacMan];
+    //Locate pacman
+    indexPacMan = game.indexOf(PACMAN);
+    indexRightPacMan = 0;
 
-if (right_val == PELLET) {
-    processPellet(game, indexRightPacMan, indexPacMan);
-} else {
-    //swap
-    temp = game[indexRightPacMan];
-    game[indexRightPacMan] = PACMAN;
-    game[indexPacMan] = temp;
-}
+    if (indexPacMan == game.length - 1) {//Far right edge case
+        indexLeftPacMan = 0;
+    } else {
+        indexRightPacMan = indexPacMan + 1;
+    }
+    right_val = game[indexRightPacMan];
 
-updateGameDisplay();
-return game;
+    if (right_val == PELLET) {//Eat a pellet
+        processPellet(game, indexRightPacMan, indexPacMan);
+    } else {
+        //swap
+        temp = game[indexRightPacMan];
+        game[indexRightPacMan] = PACMAN;
+        game[indexPacMan] = temp;
+    }
+
+    updateGameDisplay();
+    return game;
 }
 
 function moveGhost(game) {
-indexGhost = game.indexOf(GHOST);
-indexNextGhost = 0;
-leftOrRight = Math.random();
 
-if (leftOrRight >= 0.5) {
-    //move ghost right
-    if (indexGhost == game.length - 1) {
+    //Locate ghost
+    indexGhost = game.indexOf(GHOST);
     indexNextGhost = 0;
-    } else {
-    indexNextGhost = indexGhost + 1;
-    }
-} else {
-    //move ghost left
-    if (indexGhost == 0) {
-    indexNextGhost = game.length - 1;
-    } else {
-    indexGhostNext = indexGhost - 1;
-    }
-}
+    leftOrRight = Math.random(); //randomly decide to move left or right
 
-temp = game[indexNextGhost];
-game[indexNextGhost] = GHOST;
-game[indexGhost] = temp;
+    if (leftOrRight >= 0.5) { //move ghost right 
+        if (indexGhost == game.length - 1) { //Far right edge case
+        indexNextGhost = 0;
+        } else { 
+        indexNextGhost = indexGhost + 1;
+        }
+    } else { //move ghost left
+       
+        if (indexGhost == 0) { //Far left edge case
+        indexNextGhost = game.length - 1;
+        } else {
+        indexGhostNext = indexGhost - 1;
+        }
+    }
 
-updateGameDisplay();
-return game;
+    temp = game[indexNextGhost];
+    game[indexNextGhost] = GHOST;
+    game[indexGhost] = temp;
+
+    updateGameDisplay();
+    return game;
 }
 
 function processPellet(game, indexOfCrossing, indexPrev) {
-pellets++;
-score++;
-if (pellets == gameWin) {
-    return createGame(game.length + 3);
-}
-game[indexOfCrossing] = PACMAN;
-game[indexPrev] = "_";
-return game;
+    pellets++;
+    score++;
+
+    if (pellets == gameWin) { //Proceed to next level if all pellets eaten
+        return createGame(game.length + 3); //new game is 3 spaces larger
+    }
+    //remove pellet from game once eaten
+    game[indexOfCrossing] = PACMAN;
+    game[indexPrev] = "_";
+    return game;
 }
 
+
+//Controls
 function handleKeyPress(event) {
-if (event.keyCode === 37) {
-    moveLeft(game);
-} else if (event.keyCode === 39) {
-    moveRight(game);
-} else if (event.keyCode === 77) {
-    moveGhost(game);
-}
+    if (event.keyCode === 37) {//left key
+        moveLeft(game);
+    } else if (event.keyCode === 39) {//right key
+        moveRight(game);
+    } else if (event.keyCode === 77) {//M key moves ghost - meant for testing
+        moveGhost(game);
+    }
 }
 
-// Function to update the game display
+//Run game in live time
 function updateGameDisplay() {
-var gameDisplay = document.getElementById("game-display");
 
-var pelletCountDisplay = document.getElementById("pellet-display");
-var scoreDisplay = document.getElementById("score-display");
+    var gameDisplay = document.getElementById("game-display"); //gameboard
+    var pelletCountDisplay = document.getElementById("pellet-display"); //pellet counter
+    var scoreDisplay = document.getElementById("score-display"); //total score
 
-var gameString = game.join(" "); // Use join to display array elements with spaces
-var pelletString = "Pellets: " + pellets + "/" + gameWin;
-var scoreString = "Score: " + score;
+    //Strings to output
+    var gameString = game.join(" "); 
+    var pelletString = "Pellets: " + pellets + "/" + gameWin;
+    var scoreString = "Score: " + score;
 
-gameDisplay.textContent = gameString;
-pelletCountDisplay.textContent = pelletString;
-scoreDisplay.textContent = scoreString;
+    //output to HTML
+    gameDisplay.textContent = gameString;
+    pelletCountDisplay.textContent = pelletString;
+    scoreDisplay.textContent = scoreString;
 }
 
-var game = createGame(12);
-document.addEventListener("keydown", handleKeyPress);
+var game = createGame(12);//initialize game
+document.addEventListener("keydown", handleKeyPress); //listen for keyboard inputs
+
+//move ghost every 2 seconds
 setInterval(function () {
 moveGhost(game);
 }, 2000);
